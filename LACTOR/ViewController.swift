9614f3a5f0189e1999560f392190d21f011cafec
wu999 <wu999@purdue.edu>
2017-02-02 09:38:29 -0500

    Initial Commit

---
+++ LACTOR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LACTOR
+//
+//  Created by Xi Wu on 2/2/17.
+//  Copyright © 2017 EPICS WISE team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
