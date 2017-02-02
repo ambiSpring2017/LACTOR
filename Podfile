abe16bea4e80c1b66b879d39aa867376645522e5
wu999 <wu999@purdue.edu>
2017-02-02 11:36:22 -0500

    First commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'LACTOR' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  target 'LACTOR' do
+      pod 'Firebase'
+  # Pods for LACTOR
+
+end
---
+++ build/Pods.build/Debug-iphoneos/Pods-LACTOR.build/DerivedSources/Pods_LACTOR_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_LACTORVersionString[];
+ extern const double Pods_LACTORVersionNumber;
+
+ const unsigned char Pods_LACTORVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_LACTOR  PROJECT:Pods-1" "\n";
+ const double Pods_LACTORVersionNumber __attribute__ ((used)) = (double)1.;
